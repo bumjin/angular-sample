59425708c2ccb8540823264a72d123f5f2cfb35e
vtheonev <vtheonev@naver.com>
2016-07-14 14:18:12 +0900

    필터까지 반영

--- 01_hello/js/controller.js
+++ 01_hello/js/controller.js
@@ -2,6 +2,30 @@
  * Created by 김순곤 on 2016-07-09.
  */
 
-function appCtrl($scope) {
-    $scope.name = 'world';
-}
+/*var app = angular.module('myApp', [])
+app.controller('appCtrl', function($scope) {
+    $scope.name = 'wo22rld';
+})*/
+
+
+var app = angular.module('myApp', [])
+    .controller('appCtrl', function($scope) {
+    $scope.name = 'wo223rld';
+})
+
+/*function appCtrl($scope) {
+    var vm = {
+        'greet':'안녕'
+    };
+
+    $scope.name = '';
+
+    $scope.clickHandler = function() {
+        //window.alert('hello, world!')
+        $scope.name = vm.greet//'world';
+    };
+}*/
+
+/*function btnController() {
+    window.alert('hello, world!')
+}*/
--- 02_directive/assets/js/controller.js
+++ 02_directive/assets/js/controller.js
@@ -1,3 +1,65 @@
+angular.module('contactApp', [])
+    .controller('AppCtrl', function($scope) {
+        /*$scope.clickHandler = function(){
+            // window.alert('Clicked!');
+            $scope.isHidden = !$scope.isHidden;
+        };*/
+        $scope.clickHandler = clickHandler;
+        $scope.styleDemo = styleDemo;
+        $scope.styleDemo2 = styleDemo2;
+
+        function clickHandler() {
+            $scope.isHidden = !$scope.isHidden;
+        }
+
+            $scope.contacts = [
+            {
+                name: 'John Doe',
+                phone: '01234567890',
+                email: 'john@example.com'
+            },
+            {
+                name: 'Karan Bromwich',
+                phone: '09876543210',
+                email: 'karan@email.com'
+            }
+        ];
+
+        /*$scope.styleDemo = function(){
+            if(!$scope.styler){
+                return;
+            }
+
+            return {
+                background: 'red',
+                fontWeight: 'bold'
+            };
+        };*/
+        function styleDemo() {
+            if(!$scope.styler){
+                return;
+            }
+
+            return {
+                background: 'red',
+                fontWeight: 'bold'
+            };
+        }
+
+        function styleDemo2() {
+            if(!$scope.styler2){
+                return;
+            }
+
+            return {
+                background: 'blue',
+                fontWeight: 'bold'
+            };
+        }
+
+    })
+
+/*
 function AppCtrl($scope){
 
 	$scope.clickHandler = function(){
@@ -30,3 +92,4 @@ function AppCtrl($scope){
 	};
 
 }
+*/
